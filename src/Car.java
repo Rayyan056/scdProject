38cf7cba2a96646d37732da15eafdd8720fcdc9d
Rayyanirfan2 <89404776+Rayyanirfan2@users.noreply.github.com>
2022-07-01 09:52:57 +0500

    Added id attribute in Car Class

--- src/Car.java
+++ src/Car.java
@@ -8,25 +8,36 @@
  * @author Hp 820
  */
 public class Car {
+    private int id;
     private String name;
     private String model;
     private String color;
     private String plateNumber;
 
     public Car() {
+        this.id = 0;
         this.name = "";
         this.model = "";
         this.color = "";
         this.plateNumber = "";
     }
 
-    public Car(String name, String model, String color, String plateNumber) {
+    public Car(int id, String name, String model, String color, String plateNumber) {
+        this.id = id;
         this.name = name;
         this.model = model;
         this.color = color;
         this.plateNumber = plateNumber;
     }
 
+    public void setId(int id){
+        this.id = id;
+    }
+    
+    public int getId(){
+        return id;
+    }
+    
     public String getName() {
         return name;
     }
