c0123901bf41a97d79e2b33d85762d7f5ca2cbb8
Rayyanirfan2 <89404776+Rayyanirfan2@users.noreply.github.com>
2022-07-02 00:04:02 +0500

    Change List to ArrayList for get Cars function

---
+++ src/classesAndInterfaces/ShowRoom.java
@@ -0,0 +1,69 @@
+package classesAndInterfaces;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Hp 820
+ */
+import java.rmi.*;
+import java.rmi.server.*;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShowRoom extends UnicastRemoteObject implements ICarShowRoom {
+
+    DBConnection connection;
+
+    public ShowRoom() throws RemoteException{
+        
+    }
+    
+    public ShowRoom(DBConnection connection) throws RemoteException {
+        super();
+        this.connection = connection;
+    }
+
+    @Override
+    public void addCar(Car obj) {
+        String status = connection.insertData("INSERT INTO CAR(name, model, color, plateNumber) VALUES " + "('" + obj.getName() + "','" + obj.getModel() + "','" + obj.getColor() + "','" + obj.getPlateNumber() + "')");
+        System.out.println(status);
+    }
+
+    @Override
+    public void deleteCar(int id) {
+        connection.deleteData("DELETE FROM Car WHERE id="+id);
+    }
+
+    @Override
+    public void updateCar(Car obj) {
+        String query = "UPDATE CAR SET name ="+String.valueOf(obj.getName())+", model="+ String.valueOf(obj.getModel())+", color="+obj.getColor()+", plateNumber="+obj.getPlateNumber();
+        connection.updateData(query);
+    }
+
+    @Override
+    public ArrayList<Car>  getCars() {
+        ResultSet result = connection.getData("SELECT * FROM CAR");
+        ArrayList <Car> cars = new ArrayList<Car>();
+        Car obj = new Car();
+        try {
+            while(result.next()){
+                obj.setId(result.getInt("id"));
+                obj.setName(result.getString("name"));
+                obj.setModel(result.getString("model"));
+                obj.setColor(result.getString("color"));
+                obj.setPlateNumber(result.getString("plateNumber"));
+                System.out.println(obj.getName());
+                cars.add(obj);
+            }
+            return cars;
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            return null;
+        }
+    }
+}
