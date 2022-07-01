eb4e5a9b705720f1813488de879c1775466aee90
Rayyanirfan2 <89404776+Rayyanirfan2@users.noreply.github.com>
2022-07-02 04:27:37 +0500

    Added Serializible

--- src/classesAndInterfaces/Student.java
+++ src/classesAndInterfaces/Student.java
@@ -1,15 +1,17 @@
 package classesAndInterfaces;
 
+import java.io.Serializable;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author Hp 820
  */
-public class Student {
+public class Student implements Serializable {
+
     private String firstName;
     private String lastName;
     private String regNumber;
